b5d94239f3d903fb6b12dfd099edb76410e935bc
Hpsaturn <hpsaturn@gmail.com>
2018-06-06 20:52:12 -0500

    esp32 ready with HPMA115SO sensor. HardwareSerial fixed it.

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,30 @@
  * @license MIT
  */
 
+#include "Arduino.h"
 #include <hpma115S0.h>
-#include <SoftwareSerial.h>
 
 //Create an instance of software serial
-SoftwareSerial hpmaSerial(13, 15); // Feather TX, Feather RX
+HardwareSerial hpmaSerial(1);
 
 //Create an instance of the hpma115S0 library
 HPMA115S0 hpma115S0(hpmaSerial);
 
 void setup() {
   Serial.begin(9600);
-  hpmaSerial.begin(9600);
-  delay(1000);
+  hpmaSerial.begin(9600,SERIAL_8N1,13,15);
+  delay(100);
   Serial.println("Starting...");
   hpma115S0.Init();
-  delay(2000);
+  delay(100);
   hpma115S0.StartParticleMeasurement();
 }
 
 void loop() {
   unsigned int pm2_5, pm10;
   if (hpma115S0.ReadParticleMeasurement(&pm2_5, &pm10)) {
-    Serial.println("PM 2.5: " + String(pm2_5) + " ug/m3" );
-    Serial.println("PM 10: " + String(pm10) + " ug/m3" );
+    Serial.print("PM 2.5:\t" + String(pm2_5) + " ug/m3\t" );
+    Serial.println("\tPM 10:\t" + String(pm10) + " ug/m3" );
   }
-  delay(5000);
+  delay(1000);
 }

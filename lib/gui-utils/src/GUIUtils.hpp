7bda91a198ebf4042bd8141f0ab7a0510e26f4a8
Antonio Vanegas <hpsaturn@gmail.com>
2019-02-10 20:42:31 +0100

    migrated display methods to new GUIUtils library

---
+++ lib/gui-utils/src/GUIUtils.hpp
@@ -0,0 +1,48 @@
+#ifndef GUIUtils_hpp
+#define GUIUtils_hpp
+
+#include <U8g2lib.h>
+
+class GUIUtils
+{
+private:
+
+public:
+
+  GUIUtils(void) {};
+
+  unsigned int mcount, ecount = 0;
+
+  U8G2 u8g2;
+
+  // Firmware version from git rev-list command
+  String VERSION_CODE = "rev";
+#ifdef SRC_REV
+  int VCODE = SRC_REV;
+#else
+  int VCODE = 0;
+#endif
+
+  void displayInit(U8G2 &u8g2);
+
+  void showWelcome();
+
+  void displayCenterBig(String msg);
+
+  void displayBottomLine(String msg);
+
+  void displayEndLine(String msg);
+
+  void displaySensorError();
+
+  void displaySensorAvarage(int avarage);
+
+  void displaySensorData(int pm25, int pm10);
+
+  void pageStart();
+
+  void pageEnd();
+
+};
+
+#endif

bad48553b69425e532c068c90251ab41b4f0514f
Antonio Vanegas <hpsaturn@gmail.com>
2019-04-08 18:25:40 +0200

    testing influxdb and API publishing at same time, ready

--- lib/canairioapi/src/CanAirIoApi.cpp
+++ lib/canairioapi/src/CanAirIoApi.cpp
@@ -40,6 +40,8 @@ bool CanAirIoApi::write(uint16_t pm1, uint16_t pm25, uint16_t pm10, float hum, f
     char uri[32];
     sprintf(uri, "/%s", _endpoint);
 
+    if(_debug)Serial.println("\n-->[API] target: "+String(_host)+":"+String(_port)+String(uri));
+
     if(_isSecure) {
        http.begin(_host, _port, uri, _cert);
     }
@@ -54,32 +56,37 @@ bool CanAirIoApi::write(uint16_t pm1, uint16_t pm25, uint16_t pm10, float hum, f
         http.setAuthorization(_username,_password);
     }
 
-    const int capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(11);
+    const int capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(12);
     StaticJsonDocument <capacity> doc;
 
     JsonObject data = doc.createNestedObject();
 
     data["measurement"] = _nameId;
-    data["nameId"] = _nameId;
+    // data["nameId"] = _nameId;
     data["sensorId"] = _sensorId;
     JsonObject fields = data.createNestedObject("fields");
-    fields["pm1"] = pm1;
-    fields["pm25"] = pm25;
-    fields["pm10"] = pm10;
-    fields["hum"] = hum;
-    fields["tmp"] = tmp;
-    fields["lat"] = lat;
-    fields["lon"] = lon;
-    fields["alt"] = alt;
-    fields["spd"] = spd;
-    fields["stime"] = stime;
-    if(_debug)serializeJsonPretty(doc, Serial);
+    if(pm1>0)  fields["pm1"] = pm1;
+    if(pm25>0) fields["pm25"] = pm25;
+    if(pm10>0) fields["pm10"] = pm10;
+    if(hum!=0) fields["hum"] = hum;
+    if(tmp!=0) fields["tmp"] = tmp;
+    if(lat!=0) fields["lat"] = lat;
+    if(lon!=0) fields["lon"] = lon;
+    if(alt!=0) fields["alt"] = alt;
+    if(spd!=0) fields["spd"] = spd;
+    if(stime>0)fields["stime"] = stime;
+
+    if(_debug){
+        Serial.print("-->[API] payload:");
+        serializeJson(doc, Serial);
+    }
+
     String writeBuf;
     serializeJson(doc,writeBuf);
     _latestResponse = http.POST(writeBuf.c_str());
-    if(_debug)Serial.println("-->[API] response: "+String(_latestResponse));
+    if(_debug)Serial.println("\n-->[API] response: "+String(_latestResponse));
     http.end();
-    return _latestResponse == 204;
+    return _latestResponse == 200;
 }
 
 int CanAirIoApi::getResponse()

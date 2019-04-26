ed522e60341cfcd2f9d0078f1325b28d6f70981b
Antonio Vanegas <hpsaturn@gmail.com>
2019-04-26 22:01:05 +0200

    initial test of BME680 sensor with Adafruit library

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,23 @@
 #include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_AM2320.h>
+#include "Adafruit_BME680.h"
 #include <GUIUtils.hpp>
 #include <vector>
 #include "main.h"
 #include "status.h"
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; // I2C
+
+float hum_weighting = 0.25; // so hum effect is 25% of the total air quality score
+float gas_weighting = 0.75; // so gas effect is 75% of the total air quality score
+
+float hum_score, gas_score;
+float gas_reference = 250000;
+float hum_reference = 40;
+int getgasreference_count = 0;
 
 /******************************************************************************
 * S E T U P  B O A R D   A N D  F I E L D S
@@ -204,6 +217,93 @@ void humidityLoop() {
   if (v25.size() == 0) {
     getHumidityRead();
   }
+}
+
+void GetGasReference(){
+  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
+  Serial.print("new gas reference..");
+  int readings = 10;
+  for (int i = 1; i <= readings; i++){ // read gas for 10 x 0.150mS = 1.5secs
+    gas_reference += bme.readGas();
+  }
+  gas_reference = gas_reference / readings;
+}
+
+String CalculateIAQ(float score){
+  String IAQ_text = " IAQ:";
+  score = (100-score)*5;
+  if      (score >= 301)                  IAQ_text += "Hazardous";
+  else if (score >= 201 && score <= 300 ) IAQ_text += "Very Unhealthy";
+  else if (score >= 176 && score <= 200 ) IAQ_text += "Unhealthy";
+  else if (score >= 151 && score <= 175 ) IAQ_text += "Unhealthy for Sensitive Groups";
+  else if (score >=  51 && score <= 150 ) IAQ_text += "Moderate";
+  else if (score >=  00 && score <=  50 ) IAQ_text += "Good";
+  return IAQ_text;
+}
+
+void bme680loop(){
+  Serial.print("-->[BME680] Tmp:");
+
+  Serial.print(bme.readTemperature());
+  Serial.print("°C ");
+
+  Serial.print("Prs:");
+
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print("hPa ");
+
+  Serial.print("Hum:");
+  Serial.print(bme.readHumidity());
+  Serial.print("% ");
+
+  Serial.print("Gas:");
+  Serial.print(bme.readGas());
+  Serial.print("R ");
+
+  //Calculate humidity contribution to IAQ index
+  float current_humidity = bme.readHumidity();
+  if (current_humidity >= 38 && current_humidity <= 42)
+    hum_score = 0.25*100; // Humidity +/-5% around optimum 
+  else
+  { //sub-optimal
+    if (current_humidity < 38) 
+      hum_score = 0.25/hum_reference*current_humidity*100;
+    else
+    {
+      hum_score = ((-0.25/(100-hum_reference)*current_humidity)+0.416666)*100;
+    }
+  }
+  //Calculate gas contribution to IAQ index
+  float gas_lower_limit = 5000;   // Bad air quality limit
+  float gas_upper_limit = 50000;  // Good air quality limit 
+  if (gas_reference > gas_upper_limit) gas_reference = gas_upper_limit; 
+  if (gas_reference < gas_lower_limit) gas_reference = gas_lower_limit;
+  gas_score = (0.75/(gas_upper_limit-gas_lower_limit)*gas_reference -(gas_lower_limit*(0.75/(gas_upper_limit-gas_lower_limit))))*100;
+  //Combine results for the final IAQ index value (0-100% where 100% is good quality air)
+  float air_quality_score = hum_score + gas_score;
+
+  //Serial.println("Air Quality = "+String(air_quality_score,1)+"% derived from 25% of Humidity reading and 75% of Gas reading - 100% is good quality air");
+  Serial.print("IAQ:"+String(air_quality_score,1)+"% ");
+  Serial.print("HumE:"+String(hum_score/100)+" of 0.25 ");
+  Serial.print("GasE:"+String(gas_score/100)+" of 0.75 ");
+  if (bme.readGas() < 120000) Serial.print("** Poor air quality **");
+  if ((getgasreference_count++)%10==0) GetGasReference();
+  Serial.println(CalculateIAQ(air_quality_score));
+}
+
+void bme680init() {
+  bool status = bme.begin(BME680_DEFAULT_ADDRESS);
+  if (!status) {
+    Serial.println("-->[E][BME680] Could not find a valid BME280 sensor, check wiring!");
+  }
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_2X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_2X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320°C for 150 ms
+  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
+  GetGasReference();
 }
 
 /******************************************************************************
@@ -481,6 +581,7 @@ void setup() {
   gui.welcomeAddMessage("Sensors test..");
   sensorInit();
   am2320.begin();
+  bme680init();
   bleServerInit();
   gui.welcomeAddMessage("GATT server..");
   if(cfg.ssid.length()>0) gui.welcomeAddMessage("WiFi:"+cfg.ssid);
@@ -499,6 +600,7 @@ void loop(){
   sensorLoop();    // read HPMA serial data and showed it
   averageLoop();   // calculated of sensor data average
   humidityLoop();  // read AM2320
+  bme680loop();    // read BME680
   bleLoop();       // notify data to connected devices
   wifiLoop();      // check wifi and reconnect it
   apiLoop();

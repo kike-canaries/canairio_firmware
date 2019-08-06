baf574e5284f102ba9a44a535ed288b0204ca696
Daniel <42117424+danielbernalb@users.noreply.github.com>
2019-08-06 18:08:12 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -221,47 +221,37 @@ void humidityLoop() {
 /******************************************************************************
 *   B A T T E R Y   C H A R G E   S T A T U S   M E T H O D S
 ******************************************************************************/
+// IP5306_2 = pin 27 ESP32, pin 2 IP5306
+// IP5306_3 = pin 26 ESP32, pin 3 IP5306
 
 void batteryloop() {
 
   if (TTGO_TQ == 1) {
-
-// IP5306_2 = pin 27 ESP32, pin 2 IP5306
-// IP5306_3 = pin 26 ESP32, pin 3 IP5306
-
       Rdelay = 0;
-
-      digitalWrite(LED, HIGH);
+        digitalWrite(LED, HIGH);
         delayMicroseconds (50);
         digitalWrite(LED, LOW);
       
    while (digitalRead(IP5306_2) == HIGH) {   
         delayMicroseconds (100);             // Sincronization in 1
     }
-          
         delayMicroseconds (50);              // Probably double shoot in 0  
-
    while (digitalRead(IP5306_2) == HIGH) {   
         delayMicroseconds (100);             // Sincronization in 1
     }
-
-    while (digitalRead(IP5306_2) == LOW && Rdelay < 56) {
+   while (digitalRead(IP5306_2) == LOW && Rdelay < 56) {
         delayMicroseconds (100);             // Sincronization in 0
         Rdelay = Rdelay + 1; 
     }
-    
       if (Rdelay > 52) {
           chargeLevel = 0; // 0%
           Serial.println("Charge level 0%");         
           return;
       }
-
         delayMicroseconds (1600);
         digitalWrite(LED, HIGH);
         delayMicroseconds (50);
         digitalWrite(LED, LOW);
-
-        
       if (digitalRead(IP5306_2) == HIGH) {
         delayMicroseconds (100);
         if (digitalRead(IP5306_2) == HIGH) {
@@ -270,7 +260,6 @@ void batteryloop() {
           return;
         }
     }
-
       if (digitalRead(IP5306_3) == LOW) {
         delayMicroseconds (100);
         if (digitalRead(IP5306_3) == LOW) {
@@ -280,7 +269,6 @@ void batteryloop() {
         }
     }
         delayMicroseconds (1100);
-
       if (digitalRead(IP5306_3) == HIGH) {
         delayMicroseconds (100);
         if (digitalRead(IP5306_3) == HIGH) {
@@ -289,16 +277,12 @@ void batteryloop() {
           return;
         }
     }
-
      if (digitalRead(IP5306_3) == LOW) {
          chargeLevel = 2; // 50%
          Serial.println("Charge level 50%");
          return;
-       
     }
-
-  }
-          
+  }        
 }
 
 /******************************************************************************

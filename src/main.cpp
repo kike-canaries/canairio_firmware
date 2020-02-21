dafe9927ef5cf51aadd6cc577252d1f0801d31d4
danielbernalb <danielbernalb@gmail.com>
2020-02-20 23:31:51 -0500

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -35,14 +35,14 @@
 * please select board on platformio.ini file
 ******************************************************************************/
 
-#ifdef WEMOSOLED // display via i2c for WeMOS OLED board
+#ifdef WEMOSOLED // display via i2c for WeMOS OLED board & TTGO18650
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 4, 5, U8X8_PIN_NONE);
 //#elif ESP32Sboard // display via i2c for ESP32S board
 //ESP32Sboard don´t have screen
 #elif HELTEC // display via i2c for Heltec board
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 15, 4, 16);
-#elif TTGO18650 // display via i2c for TTGO18650
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 4, 5, U8X8_PIN_NONE);
+//#elif TTGO18650 // display via i2c for TTGO18650
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 4, 5, U8X8_PIN_NONE);
 #elif TTGO_TQ // display via i2c for TTGO_TQ
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 4, 5);
 #else       // display via i2c for D1MINI board
@@ -51,13 +51,13 @@ U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0,U8X8_PIN_NONE,U8X8_PIN_NONE,U8X8_PIN
 
 // HPMA115S0 sensor config
 #ifdef WEMOSOLED
-#define HPMA_RX 13   // config for Wemos board, TTGO18650
+#define HPMA_RX 13   // config for Wemos board & TTGO18650
 #define HPMA_TX 15
 #elif ESP32Sboard
 #define HPMA_RX 27  // config for ESP32S board
 #define HPMA_TX 25
 #elif HELTEC
-#define HPMA_RX 13  // config for Heltec board, ESP32S & ESPDUINO-32 & TTGO18650
+#define HPMA_RX 13  // config for Heltec board, ESP32S & ESPDUINO-32
 #define HPMA_TX 12
 //#elif TTGO18650
 //#define HPMA_RX 18  // config for TTGO18650 board

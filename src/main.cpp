f291a2866c7a34ca7fc180d681982d790cb239b8
danielbernalb <danielbernalb@gmail.com>
2020-02-24 13:06:35 -0500

    correction error merge

--- src/main.cpp
+++ src/main.cpp
@@ -161,12 +161,12 @@ void sensorLoop(){
     Serial.println("-->[E][HPMA] disconnected ?"); 
     delay(500);  // waiting for sensor..
   }
-  if (txtMsg[0] == 66) {
-    if (txtMsg[1] == 77) {
+  if (txtMsg[0] == 02) {
+  //  if (txtMsg[1] == 77) {
       Serial.print("-->[HPMA] read > done!");
       statusOn(bit_sensor);
-      unsigned int pm25 = txtMsg[6] * 256 + byte(txtMsg[7]);
-      unsigned int pm10 = txtMsg[8] * 256 + byte(txtMsg[9]);
+      unsigned int pm25 = txtMsg[6] * 256 + byte(txtMsg[5]);
+      unsigned int pm10 = txtMsg[10] * 256 + byte(txtMsg[9]);
       if(pm25<1000&&pm10<1000){
         gui.displaySensorAvarage(apm25);  // it was calculated on bleLoop()
         #ifdef TTGO_TQ
@@ -180,8 +180,8 @@ void sensorLoop(){
       else wrongDataState();
     }
     else wrongDataState();
-  }
-  else wrongDataState();
+//  }
+//  else wrongDataState();
 }
 
 void statusLoop(){

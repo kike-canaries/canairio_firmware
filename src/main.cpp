f768a826d3ef7850b060738bc7e15243ca6ee207
danielbernalb <danielbernalb@gmail.com>
2020-04-21 12:58:59 -0500

    correct serial frame

--- src/main.cpp
+++ src/main.cpp
@@ -154,7 +154,7 @@ void sensorLoop(){
 
 #ifdef PANASONIC
   if (txtMsg[0] == 02) {
-      Serial.print("done!");
+      Serial.print("-->[HPMA] read > done!");
       statusOn(bit_sensor);
       unsigned int pm25 = txtMsg[6] * 256 + byte(txtMsg[5]);
       unsigned int pm10 = txtMsg[10] * 256 + byte(txtMsg[9]);
@@ -175,7 +175,7 @@ void sensorLoop(){
 #else
   if (txtMsg[0] == 66) {
     if (txtMsg[1] == 77) {
-      Serial.print("done!");
+      Serial.print("-->[HPMA] read > done!");
       statusOn(bit_sensor);
       unsigned int pm25 = txtMsg[6] * 256 + byte(txtMsg[7]);
       unsigned int pm10 = txtMsg[8] * 256 + byte(txtMsg[9]);

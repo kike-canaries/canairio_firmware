c7e7bb66a89b64ad81d5afaa6868814e710d4bb4
Hpsaturn <hpsaturn@gmail.com>
2020-06-17 16:19:08 -0500

    Sensirion Serial Support, Format Text with VScode
    1. New support for Sensirion sensor using UART, same ports (16 & 17).
    2. Formated Documments by VScode

--- src/main.h
+++ src/main.h
@@ -4,50 +4,51 @@ using namespace std;
 HardwareSerial hpmaSerial(1);
 HPMA115S0 hpma115S0(hpmaSerial);
 
-vector<unsigned int> v25;      // for average
-vector<unsigned int> v10;      // for average
-unsigned int apm25 = 0;        // last PM2.5 average
-unsigned int apm10 = 0;        // last PM10 average
-#define SENSOR_RETRY  1000     // Sensor read retry
+vector<unsigned int> v25; // for average
+vector<unsigned int> v10; // for average
+unsigned int apm25 = 0;   // last PM2.5 average
+unsigned int apm10 = 0;   // last PM10 average
+#define SENSOR_RETRY 1000 // Sensor read retry
 
 // Sensirion SPS30 sensor
-#define SP30_COMMS I2C_COMMS        // UART OR I2C  !!!!!!!!!!!!!!!!!!!
-//int PM25 = 0;
+#define SP30_COMMS SERIALPORT2 // UART OR I2C
+uint8_t ret, error_cnt = 0;
+struct sps_values val;
 
 // Humidity sensor
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
-float humi = 0.0;              // % Relative humidity 
-float temp = 0.0;              // Temperature (C)
+float humi = 0.0; // % Relative humidity
+float temp = 0.0; // Temperature (C)
 
 // Battery level
 unsigned int chargeLevel = 0;
 #ifdef TTGO_TQ
-const int IP5306_2 = 27;     // PIN2 IP5306
-const int IP5306_3 = 14;     // PIN3 IP5306
+const int IP5306_2 = 27; // PIN2 IP5306
+const int IP5306_3 = 14; // PIN3 IP5306
 unsigned int Rdelay = 0;
 #endif
 
 // WiFi fields
-#define WIFI_RETRY_CONNECTION    20
+#define WIFI_RETRY_CONNECTION 20
 bool dataSendToggle;
 bool wifiOn;
 int rssi = 0;
 
 // Bluetooth fields
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharactData = NULL;
-BLECharacteristic* pCharactConfig = NULL;
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharactData = NULL;
+BLECharacteristic *pCharactConfig = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-#define SERVICE_UUID        "c8d1d262-861f-4082-947e-f383a259aaf3"
-#define CHARAC_DATA_UUID    "b0f332a8-a5aa-4f3f-bb43-f99e7791ae01"
-#define CHARAC_CONFIG_UUID  "b0f332a8-a5aa-4f3f-bb43-f99e7791ae02"
+#define SERVICE_UUID "c8d1d262-861f-4082-947e-f383a259aaf3"
+#define CHARAC_DATA_UUID "b0f332a8-a5aa-4f3f-bb43-f99e7791ae01"
+#define CHARAC_CONFIG_UUID "b0f332a8-a5aa-4f3f-bb43-f99e7791ae02"
 
 // CanAirIO API fields
 CanAirIoApi api(false);
 
 // InfluxDB fields
-#define IFX_RETRY_CONNECTION   2
+#define IFX_RETRY_CONNECTION 2
 InfluxArduino influx;
 
 // Config and settings handler
@@ -64,7 +65,7 @@ unsigned int resetvar = 0;
 
 // some prototypes
 bool wifiCheck();
-void wifiConnect(const char* ssid, const char* pass);
+void wifiConnect(const char *ssid, const char *pass);
 void wifiInit();
 void wifiStop();
 void wifiRestart();

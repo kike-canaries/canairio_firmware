1a4fecd4b44a09fac6f28e9b3cfc261ebc0a7456
danielbernalb <danielbernalb@gmail.com>
2020-07-31 20:42:33 -0500

    Added PM2.5 float, minor changes

--- src/main.h
+++ src/main.h
@@ -5,10 +5,13 @@ HPMA115S0 hpma115S0(hpmaSerial);
 
 vector<unsigned int> v25;      // for average
 vector<unsigned int> v10;      // for average
+vector<float> v25f;            // for average
 unsigned int apm25 = 0;        // last PM2.5 average
 unsigned int apm10 = 0;        // last PM10 average
+float apm25f = 0.0;            // last PM2.5 float average
 unsigned int pm25 = 0;
 unsigned int pm10 = 0;
+float pm25f = 0.0;
 #define SENSOR_RETRY  1000     // Sensor read retry
 
 // Sensirion SPS30 sensor

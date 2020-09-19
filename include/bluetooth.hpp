a7d2a8573bfa7f1ec754aa10027ea1bd00be45e0
Antonio Vanegas <hpsaturn@gmail.com>
2020-09-19 11:12:17 +0200

    migrated from CanAirIO firmware @hpsaturn

---
+++ include/bluetooth.hpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <ConfigApp.hpp>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <pmsensor.hpp>
+#include <wifi.hpp>
+#include <gui.hpp>
+#include <hal.hpp>
+
+#define SERVICE_UUID        "c8d1d262-861f-4082-947e-f383a259aaf3"
+#define CHARAC_DATA_UUID    "b0f332a8-a5aa-4f3f-bb43-f99e7791ae01"
+#define CHARAC_CONFIG_UUID  "b0f332a8-a5aa-4f3f-bb43-f99e7791ae02"
+
+void bleLoop();
+void bleServerInit();
+bool bleIsConnected();

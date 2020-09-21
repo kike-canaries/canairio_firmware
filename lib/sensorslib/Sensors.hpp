2e08b6bcaa4001e24581b103a7afd3c6519bcab6
Hpsaturn <hpsaturn@gmail.com>
2020-09-21 10:22:40 +0200

    added onerror callback sendor method. Some minors.

--- lib/sensorslib/Sensors.hpp
+++ lib/sensorslib/Sensors.hpp
@@ -31,24 +31,51 @@ using namespace std;
 #define HPMA_TX 16
 #endif
 
-#define LED 2
-
 #define I2C_SDA_PIN 21
 #define I2C_SCL_PIN 22
-
-#define PMS_RX 13  // config for D1MIN1 board
-#define PMS_TX 14
-#define PMS_EN 27   // Panasonic enable pin (2N2222A switch to booster module)
 
 #define SENSOR_INTERVAL 1000 * 60 * 3  // 3 minutes => more is better for the battery
 #define SENSOR_SAMPLE 1000 * 35        // 30 seconds => less is better for the battery
 #define SENSOR_RETRY 1000              // Sensor read retry
+
 // Sensirion SPS30 sensor
 #define SP30_COMMS SERIALPORT2 // UART OR I2C
 
+typedef void (*voidCbFn)(void);
 
 class Sensors
 {
+
+    public: 
+
+    voidCbFn _onErrorCb;
+
+    void init();
+    void loop();
+    bool isDataReady();
+    void setErrorCallBack(voidCbFn cb);
+
+    uint16_t getPM1();
+    uint16_t getPM25();
+    uint16_t getPM10();
+
+    float getTemperature();
+    float getHumidity();
+    float getPressure();
+    float getAltitude();
+    float getGas();
+
+    String getFormatTemp();
+    String getFormatPress();
+    String getFormatHum();
+    String getFormatGas();
+    String getFormatAlt();
+
+    String getStringPM1();
+    String getStringPM25();
+    String getStringPM10();
+
+
     private:
 
     // Sensirion SPS30 sensor
@@ -76,31 +103,7 @@ class Sensors
     void pmSensirionErrtoMess(char *mess, uint8_t r);
     void pmSensirionErrorloop(char *mess, uint8_t r);
 
-    public: 
-
-    void init();
-    void loop();
-    bool isDataReady();
-
-    uint16_t getPM1();
-    uint16_t getPM25();
-    uint16_t getPM10();
-
-    float getTemperature();
-    float getHumidity();
-    float getPressure();
-    float getAltitude();
-    float getGas();
-
-    String getFormatTemp();
-    String getFormatPress();
-    String getFormatHum();
-    String getFormatGas();
-    String getFormatAlt();
-
-    String getStringPM1();
-    String getStringPM25();
-    String getStringPM10();
+    
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_SENSORSHANDLER)

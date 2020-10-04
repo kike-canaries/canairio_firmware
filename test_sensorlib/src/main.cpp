a47bd645635b0bdf754d0f803b56a683c94a8ca9
Antonio Vanegas <hpsaturn@gmail.com>
2020-10-04 13:56:19 +0200

    added functional example for sensorlib test

--- test_sensorlib/src/main.cpp
+++ test_sensorlib/src/main.cpp
@@ -2,24 +2,17 @@
  * @file main.cpp
  * @author Antonio Vanegas @hpsaturn
  * @date June 2018 - 2020
- * @brief Particle meter sensor on ESP32 with bluetooth GATT notify server
+ * @brief Particle meter sensor tests
  * @license GPL3
  */
 
 #include <Arduino.h>
-
 #include <Sensors.hpp>
 
-void displayGUI() {
-    static uint_fast64_t timeStampGUI = 0;   // timestamp for GUI refresh
-    if ((millis() - timeStampGUI > 1000)) {  // it should be minor than sensor loop
-        timeStampGUI = millis();
-        Serial.println("-->[MAIN] PM2.5 value: "+sensors.getPM25());
-    }
-}
-
 void onSensorDataOk() {
-    gui.displaySensorLiveIcon();  // all sensors read are ok
+    Serial.print("-->[MAIN] PM1.0: "+sensors.getStringPM1());
+    Serial.print  (" PM2.5: " + sensors.getStringPM25());
+    Serial.println(" PM10: " + sensors.getStringPM10());
 }
 
 /******************************************************************************
@@ -32,12 +25,12 @@ void setup() {
     Serial.println("\n== Sensor test setup ==\n");
 
     sensors.setOnDataCallBack(&onSensorDataOk);  // all data read callback
-    sensors.setSampleTime(5);            // config sensors sample time
-    sensors.init(sensors.Sensirion);             // start all sensors
-    // if(sensors.isPmSensorConfigured())
-        // gui.welcomeRepeatMessage(sensors.getPmDeviceSelected());
-    // else 
-        // gui.welcomeRepeatMessage("Detection !FAIL!");
+    sensors.setSampleTime(5);                       // config sensors sample time interval
+    sensors.init(sensors.Sensirion);                // start all sensors and force to PM sensor to Sensirion
+
+    if(sensors.isPmSensorConfigured())
+        Serial.println("-->[SETUP] Sensor configured: " + sensors.getPmDeviceSelected());
+
     delay(500);
 }
 

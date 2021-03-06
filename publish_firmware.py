3a4636526585011e532ad61c0d725b19dfef1d13
Antonio Vanegas <hpsaturn@gmail.com>
2021-03-06 15:40:38 +0100

    custom migration to publish script on OTA platformio

--- publish_firmware.py
+++ publish_firmware.py
@@ -1,59 +1,54 @@
-# Copyright (c) 2014-present PlatformIO <contact@platformio.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# build.py
+# pre-build script, setting up build environment and fetch hal file for user's board
 
-import requests
 import sys
 import os
+import os.path
+import requests
 from os.path import basename
-
-Import("env")
-
-# get api token, as the "old way" does not seem to work with sysenv for windows 
-my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-apiToken = defines.get("BINTRAY_TOKEN") # this works
+from platformio import util
+from SCons.Script import DefaultEnvironment
 
 try:
     import configparser
 except ImportError:
     import ConfigParser as configparser
 
-project_config = configparser.ConfigParser()
-project_config.read("platformio.ini")
-version = project_config.get("common", "release_version")
-bintray_config = {k: v for k, v in project_config.items("bintray")}
+# get platformio environment variables
+env = DefaultEnvironment()
+config = configparser.ConfigParser()
+config.read("platformio.ini")
 
+# get platformio source path
+srcdir = env.get("PROJECTSRC_DIR")
 
-#
-# Push new firmware to the Bintray storage using API
-#
+# get bintray user credentials from ota key file
+bintray_config = {k: v for k, v in config.items("bintray")}
+user = bintray_config.get("user")
+repository = bintray_config.get("repository")
+apitoken = bintray_config.get("api_token")
+package = bintray_config.get("package")
 
+# get bintray upload parameters from platformio environment
+version = config.get("common", "release_version")
 
+# put bintray user credentials to platformio environment
+env.Replace(BINTRAY_USER=user)
+env.Replace(BINTRAY_REPO=repository)
+env.Replace(BINTRAY_API_TOKEN=apitoken)
 
-def publish_firmware(source, target, env):
+# function for pushing new firmware to bintray storage using API
+def publish_bintray(source, target, env):
     firmware_path = str(source[0])
     firmware_name = basename(firmware_path)
+    url = "/".join([
+        "https://api.bintray.com", "content",
+        user, repository, package, version, firmware_name
+    ])
 
     print("Uploading {0} to Bintray. Version: {1}".format(
         firmware_name, version))
-
-    url = "/".join([
-        "https://api.bintray.com", "content",
-        bintray_config.get("user"),
-        bintray_config.get("repository"),
-        bintray_config.get("package"), version, firmware_name
-    ])
+    print(url)
 
     headers = {
         "Content-type": "application/octet-stream",
@@ -62,12 +57,12 @@ def publish_firmware(source, target, env):
     }
 
     r = None
+    
     try:
         r = requests.put(url,
                          data=open(firmware_path, "rb"),
                          headers=headers,
-                         auth=(bintray_config.get("user"),
-                               apiToken))
+                         auth=(user,apitoken))
         r.raise_for_status()
     except requests.exceptions.RequestException as e:
         sys.stderr.write("Failed to submit package: %s\n" %
@@ -76,6 +71,7 @@ def publish_firmware(source, target, env):
 
     print("The firmware has been successfuly published at Bintray.com!")
 
-
-# Custom upload command and program name
-env.Replace(PROGNAME="firmware_v_%s" % version, UPLOADCMD=publish_firmware)
+# put build file name and upload command to platformio environment
+env.Replace(
+    PROGNAME="firmware_" + package + "_v%s" % version,
+    UPLOADCMD=publish_bintray)

7e1a86c304b267b471a11023e0dfebfeaae2ce61
Hpsaturn <hpsaturn@gmail.com>
2021-05-14 13:41:52 +0200

    added and implemented same methods for OLED variant like TFT

--- lib/gui-utils-oled/src/GUIUtils.hpp
+++ lib/gui-utils-oled/src/GUIUtils.hpp
@@ -2,14 +2,20 @@
 #define GUIUtils_hpp
 
 #include <U8g2lib.h>
+#include "hal.hpp"
 
+class GUIUserPreferencesCallbacks; 
 class GUIUtils {
    public:
     GUIUtils(void){};
 
     void displayInit();
 
+    void setCallbacks(GUIUserPreferencesCallbacks* pCallbacks);
+
     void showWelcome();
+
+    void showMain();
 
     void showProgress(unsigned int progress, unsigned int total);
 
@@ -17,11 +23,9 @@ class GUIUtils {
     
     void welcomeRepeatMessage(String msg);
 
-    void displaySensorAverage(int average, int deviceType);
+    void displayGUIStatusFlags();
 
-    void displaySensorData(int mainValue, int chargeLevel, float humi, float temp, int rssi, int deviceType);
-
-    void displayStatus(bool wifiOn, bool bleOn, bool blePair);
+    void displayMainValues();
 
     void displayDataOnIcon();
 
@@ -34,6 +38,22 @@ class GUIUtils {
     void pageStart();
 
     void pageEnd();
+
+    void clearScreen();
+    
+    void setSensorData(int mainValue, int chargeLevel, float humi, float temp, int rssi, int deviceType);
+
+    void setGUIStatusFlags(bool wifiOn, bool bleOn, bool blePair);
+
+    void setBrightness(uint32_t value);
+
+    void setWifiMode(bool enable);
+
+    void setSampleTime(int time);
+
+    void setTrackValues(float speed, float distance);
+
+    void setTrackTime(int h, int m, int s);
 
     String getFirmwareVersionCode ();
 
@@ -53,6 +73,34 @@ class GUIUtils {
 
     int dh = 0;  // display height
 
+    int _rssi = 0;
+
+    int _deviceType = 0;
+
+    float _humi = 0.0;
+
+    float _temp = 0.0;
+
+    int _mainValue = 0;
+
+    int _average = 0;
+
+    bool _wifi_enable;
+
+    bool _wifiOn;
+
+    bool _bleOn;
+
+    bool _blePair;
+
+    bool isNewData;
+
+    TaskHandle_t xHandle;
+
+    bool taskGUIrunning;
+
+    void displaySensorAverage(int average, int deviceType);
+
     void displayCenterBig(String msg, int deviceType);
 
     void displayEmoticonLabel(int numsmile, String msg);
@@ -60,6 +108,19 @@ class GUIUtils {
     void displayBigEmoticon(String msg);
 
     void displayBigLabel(int cursor, String msg);
+
+    void setupGUITask();
+
+};
+
+class GUIUserPreferencesCallbacks {
+public:
+    virtual ~GUIUserPreferencesCallbacks () {};
+    virtual void onWifiMode(bool enable);
+	virtual void onBrightness(int value);
+    virtual void onColorsInverted(bool enable);
+    virtual void onSampleTime(int time);
+    virtual void onCalibrationReady();
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_GUIHANDLER)

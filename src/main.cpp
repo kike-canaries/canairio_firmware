21917690a2725c3edb28d49fb88c02b5784d2370
Antonio Vanegas <hpsaturn@gmail.com>
2021-06-22 14:35:30 +0200

    added new preference: enable debug mode (via BLE)

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ void startingSensors() {
     sensors.setOnDataCallBack(&onSensorDataOk);     // all data read callback
     sensors.setOnErrorCallBack(&onSensorDataError); // on data error callback
     sensors.setSampleTime(cfg.stime);               // config sensors sample time
-    sensors.setDebugMode(false);                    // [optional] debug mode
+    sensors.setDebugMode(cfg.devmode);                    // [optional] debug mode
     sensors.init(cfg.getSensorType());              // start all sensors and
                                                     // try to detect configured PM sensor.
                                                     // Sensors PM2.5 supported: Panasonic, Honeywell, Plantower and Sensirion

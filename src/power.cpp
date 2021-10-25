7cf1fbd7f9e96d6f4697e37143ed6cf89d4e04ba
BRKMK <84018539+BRKMK@users.noreply.github.com>
2021-10-25 22:36:22 +0200

    faltaban

---
+++ src/power.cpp
@@ -0,0 +1,16 @@
+#include <power.h>
+#include <driver/adc.h>
+
+void PowerDeepSleepTimer(int seconds)
+{
+    esp_sleep_enable_timer_wakeup(seconds * 1000000);
+    esp_deep_sleep_start();
+}
+
+void PowerLightSleepTimer(int seconds)
+{
+    //adc_power_off();
+
+    esp_sleep_enable_timer_wakeup(seconds * 1000000);
+    esp_light_sleep_start();
+}

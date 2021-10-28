856fcf5bd3ac41389e78f2d234a43671353e05f4
Antonio Vanegas <hpsaturn@gmail.com>
2021-10-28 14:52:24 +0200

    added lines to disable all radious before deep-sleep

--- src/power.cpp
+++ src/power.cpp
@@ -1,16 +1,15 @@
 #include <power.h>
-#include <driver/adc.h>
 
-void PowerDeepSleepTimer(int seconds)
-{
+void PowerDeepSleepTimer(int seconds) {
+    esp_bluedroid_disable();
+    esp_bt_controller_disable();
+    esp_wifi_stop();
+    // esp_sleep_enable_ext0_wakeup(GPIO_NUM_35, 0);
     esp_sleep_enable_timer_wakeup(seconds * 1000000);
     esp_deep_sleep_start();
 }
 
-void PowerLightSleepTimer(int seconds)
-{
-    //adc_power_off();
-
+void PowerLightSleepTimer(int seconds) {
     esp_sleep_enable_timer_wakeup(seconds * 1000000);
     esp_light_sleep_start();
 }
--- src/power.h
+++ src/power.h
@@ -1,8 +1,9 @@
-#pragma once
 #ifndef _POWER_H
 #define _POWER_H
 
-#include <Arduino.h>
+#include <esp_bt_main.h>
+#include <esp_bt.h>
+#include <esp_wifi.h>
 
 void PowerDeepSleepTimer(int);
 void PowerLightSleepTimer(int);

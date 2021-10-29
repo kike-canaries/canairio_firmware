bfdc8c232477f4c893dab7e849eb654a9fa60e4d
Hpsaturn <hpsaturn@gmail.com>
2021-10-29 13:02:14 +0200

    migrated ADC rutine to prepare shutdown

--- src/power.h
+++ src/power.h
@@ -1,9 +1,11 @@
 #ifndef _POWER_H
 #define _POWER_H
 
+#include <Arduino.h>
 #include <esp_bt_main.h>
 #include <esp_bt.h>
 #include <esp_wifi.h>
+#include "hal.hpp"
 
 void powerDeepSleepButton();
 void powerDeepSleepTimer(int);

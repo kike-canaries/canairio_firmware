dd3a11c328c827c396f556682b00fea77f5d8adf
Hpsaturn <hpsaturn@gmail.com>
2021-11-09 12:12:51 +0100

    added new presset PaxEnable for PaxCounter variable

--- lib/configlib/ConfigApp.cpp
+++ lib/configlib/ConfigApp.cpp
@@ -35,6 +35,7 @@ void ConfigApp::reload() {
     toffset = preferences.getFloat("toffset", 0.0);
     altoffset = preferences.getFloat("altoffset", 0.0);
     devmode = preferences.getBool("debugEnable", false);
+    pax_enable = preferences.getBool("paxEnable", true);
     i2conly = preferences.getBool("i2conly", false);
 
     preferences.end();
@@ -54,6 +55,7 @@ String ConfigApp::getCurrentConfig() {
     doc["ifxpt"] = preferences.getUInt("ifxpt", ifx.pt);     // influxdb sensor tags
     doc["geo"] = preferences.getString("geo", "");           // influxdb GeoHash tag
     doc["denb"] = preferences.getBool("debugEnable", false); // debug mode enable
+    doc["penb"] = preferences.getBool("paxEnable", true);    // PaxCounter enable
     doc["i2conly"] = preferences.getBool("i2conly", false);  // force only i2c sensors
     doc["toffset"] = preferences.getFloat("toffset", 0.0);   // temperature offset
     doc["altoffset"] = preferences.getFloat("altoffset",0.0);// altitude offset
@@ -236,6 +238,13 @@ bool ConfigApp::debugEnable(bool enable) {
     return true;
 }
 
+bool ConfigApp::paxEnable(bool enable) {
+    saveBool("paxEnable", enable);
+    pax_enable = enable;
+    Serial.println("-->[CONF] updating PaxCounter mode: " + String(enable));
+    return true;
+}
+
 bool ConfigApp::saveI2COnly(bool enable) {
     saveBool("i2conly", enable);
     i2conly = enable;
@@ -278,6 +287,7 @@ bool ConfigApp::save(const char *json) {
         if (act.equals("ist")) return ifxdbEnable(doc["ienb"].as<bool>());
         if (act.equals("dst")) return debugEnable(doc["denb"].as<bool>());
         if (act.equals("i2c")) return saveI2COnly(doc["i2conly"].as<bool>());
+        if (act.equals("pst")) return paxEnable(doc["penb"].as<bool>());
         if (act.equals("rbt")) reboot();
         if (act.equals("cls")) clear();
         if (act.equals("clb")) performCO2Calibration();
@@ -324,6 +334,10 @@ String ConfigApp::getDeviceIdShort() {
 
 bool ConfigApp::isWifiEnable() {
     return wifi_enable;
+}
+
+bool ConfigApp::isPaxEnable() {
+    return pax_enable;
 }
 
 bool ConfigApp::isIfxEnable() {

ca98d6076fb16fbae00136e97969db00c391286a
Hpsaturn <hpsaturn@gmail.com>
2021-11-22 13:57:02 +0100

    some code refactors and validations for bad wifi conditions

--- include/wifi.hpp
+++ include/wifi.hpp
@@ -11,6 +11,7 @@
 #define PUBLISH_INTERVAL 30       // publish to cloud each 30 seconds
 #define WIFI_RETRY_CONNECTION 30  // 30 seconds wait for wifi connection
 #define IFX_RETRY_CONNECTION 5    // influxdb publish retry 
+#define MQTT_RETRY_CONNECTION 5   // mqtt publish retry
 
 void otaLoop();
 void otaInit();

8e42723dea675ad5ca18483e68842f720f48dd07
Hpsaturn <hpsaturn@gmail.com>
2021-11-25 15:13:18 +0100

    first version of HASS working (issue: temp value)

--- include/cloud_hass.hpp
+++ include/cloud_hass.hpp
@@ -2,5 +2,10 @@
 #define HASS_TOPIC "measurement"
 #define HASS_PORT 1883
 
+#define HPREFIX "homeassistant/"
+#define HCOMP "sensor/"
+#define TOPIC_CONF "config"
+#define TOPIC_STATE "state"
+
 void hassLoop ();
 void hassInit ();

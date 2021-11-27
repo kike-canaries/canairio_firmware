9ea0bd5a14755b1043baa39d5362fe85fd796e5f
Hpsaturn <hpsaturn@gmail.com>
2021-11-27 20:08:52 +0100

    rev852 "multi sensor" working on Hass integration (disabled for now)

--- src/cloud_hass.cpp
+++ src/cloud_hass.cpp
@@ -12,45 +12,39 @@ String getRootTopic() {
     return String(HPREFIX) + String(HCOMP) + getHostId();
 }
 
-String getStateTopic() {
-    return getRootTopic()+"/"+String(TOPIC_STATE);
+String getStateTopic(String dclass) {
+    return getRootTopic()+"/"+dclass+"/"+String(TOPIC_STATE);
 }
 
 String getConfTopic(String name) {
     return getRootTopic()+"/"+name+"/"+String(TOPIC_CONF);
 }
 
-void hassPubSensorPayload() {
-    char MQTT_message[MQTT_BUFFER_SIZE];
-
-    int deviceType = sensors.getPmDeviceTypeSelected();
+void hassPubSensorPayload(String dclass) {
 
     float humi = sensors.getHumidity();
     if (humi == 0.0) humi = sensors.getCO2humi();
     float temp = sensors.getTemperature();
     if (temp == 0.0) temp = sensors.getCO2temp();
 
-    // if (deviceType <= 3) {
-    //     sprintf(MQTT_message, "{id: %s, pm1: %d, pm25: %d, pm10: %d, tmp: %f, hum: %f, geo: %s}",
-    //             cfg.getStationName().c_str(),
-    //             sensors.getPM1(),
-    //             sensors.getPM25(),
-    //             sensors.getPM10(),
-    //             temp,
-    //             humi,
-    //             cfg.geo.c_str());
-    // } else {
-    //     sprintf(MQTT_message, "{ CO2: %d, humidity: %f, temperature: %f }",
-    //             sensors.getCO2(),
-    //             humi,
-    //             temp
-    //     );
-    // }
-    if (clientHass.publish(getStateTopic().c_str(), String(humi))) return;
+    String output = "";
+
+    if (dclass.equals("carbon_dioxide")) output = String(sensors.getCO2());
+    else if (dclass.equals("humidity")) output = String(humi);
+    else if (dclass.equals("temperature")) output = String(temp);
+    else if (dclass.equals("pressure")) output = String(sensors.getPressure());
+    else if (dclass.equals("altitude")) output = String(sensors.getAltitude());
+    else if (dclass.equals("gas")) output = String(sensors.getGas());
+    else if (dclass.equals("pm1")) output = String(sensors.getPM1());
+    else if (dclass.equals("pm25")) output = String(sensors.getPM25());
+    else if (dclass.equals("pm4")) output = String(sensors.getPM4());
+    else if (dclass.equals("pm10")) output = String(sensors.getPM10());
+ 
+    if (clientHass.publish(getStateTopic(dclass).c_str(), output)) return;
     Serial.printf("[E][MQTT] last error: %d\n",clientHass.lastError());
 }
 
-void publishDiscoveryPayload(String name, String dclass, String unit, String tpl) {
+void publishDiscoveryPayload(String name, String dclass, String unit) {
     StaticJsonDocument<MQTT_BUFFER_SIZE> doc;
     doc["name"] = getHostId()+name;
     JsonObject device = doc.createNestedObject("device");
@@ -60,7 +54,7 @@ void publishDiscoveryPayload(String name, String dclass, String unit, String tpl
     device["sw_version"] = "v"+String(VERSION)+" rev"+String(REVISION);
     JsonArray identifiers = device.createNestedArray("identifiers");
     identifiers.add(getHostId()+name);
-    doc["state_topic"] = getStateTopic();
+    doc["state_topic"] = getStateTopic(dclass);
     // JsonArray attributes = doc.createNestedArray("json_attributes");
     // attributes.add(dclass);
     // doc["json_attributes_topic"] = getStateTopic();
@@ -74,25 +68,44 @@ void publishDiscoveryPayload(String name, String dclass, String unit, String tpl
      
     if (clientHass.publish(getConfTopic(dclass).c_str(), MQTT_message, n)) return;
     Serial.printf("[E][MQTT] last error: %d\n",clientHass.lastError());
+
 }
 
 void hassPubDiscoveryTemp() {
-    publishDiscoveryPayload("temperature", "temperature", "°C", "{{ value_json.temparatue | default(0) }}");
+    publishDiscoveryPayload("temperature", "temperature", "°C");
 }
 
 void hassPubDiscoveryHumi() {
-    publishDiscoveryPayload("humidity", "humidity", "%", "{{  humidity.humidity  }}");
+    publishDiscoveryPayload("humidity", "humidity", "%");
 }
 
 void hassPubDiscoveryCO2() {
-    publishDiscoveryPayload("carbon_dioxide", "carbon_dioxide", "ppm", "{{ value_json.CO2 |default(0)  }}");
+    publishDiscoveryPayload("carbon_dioxide", "carbon_dioxide", "ppm");
+}
+
+void hassPubDiscoveryPM25() {
+    publishDiscoveryPayload("pm25", "pm25", "µg/m³");
+}
+
+void hassPubAllSensors() {
+    hassPubSensorPayload("temperature");
+    hassPubSensorPayload("humidity");
+    hassPubSensorPayload("carbon_dioxide");
+    hassPubSensorPayload("pm25");
+}
+
+void hassRegisterSensors() {
+    hassPubDiscoveryTemp();
+    hassPubDiscoveryHumi();
+    hassPubDiscoveryCO2();
+    hassPubDiscoveryPM25();
 }
 
 void hassPublish() {
     static uint_fast64_t mqttTimeStamp = 0;
     if (millis() - mqttTimeStamp > cfg.stime * 1000) {
         mqttTimeStamp = millis(); 
-        hassPubSensorPayload();
+        hassPubAllSensors();
     }
 }
 
@@ -113,20 +126,25 @@ void hassConnect() {
             return;
         }
         mqttHassDelayedStamp = millis();
-        // hassPubDiscoveryTemp();
-        hassPubDiscoveryHumi();
-        // hassPubDiscoveryCO2();
+        hassRegisterSensors();
+
         Serial.println("\tconnected!");
     }
 }
 
+bool isHassEnable() {
+    return !cfg.hassip.isEmpty();
+}
+
 void hassInit() { 
-    clientHass.begin(HASS_HOST, HASS_PORT, netHass);
+    if (!isHassEnable()) return;
+    clientHass.begin(cfg.hassip.c_str(), cfg.hasspt, netHass);
     mqttHassDelayedStamp = millis() - MQTT_DELAYED_TIME * 1000;
     hassConnect();
 }
 
 void hassLoop () {
+    if (!isHassEnable()) return;
     if(!WiFi.isConnected()) return; 
     clientHass.loop();
     delay(10);

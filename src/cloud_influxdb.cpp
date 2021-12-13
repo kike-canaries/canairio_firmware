26e3a071b72e6a00d6a5b4f24bf5d2ebd94dad4b
roberbike <roberbike@gmail.com>
2021-12-13 10:42:59 +0100

    if ble connected , no deepsleep

--- src/cloud_influxdb.cpp
+++ src/cloud_influxdb.cpp
@@ -1,5 +1,6 @@
 #include <cloud_influxdb.hpp>
 #include <wifi.hpp>
+#include <bluetooth.hpp>
 
 /******************************************************************************
 *   I N F L U X D B   M E T H O D S
@@ -57,6 +58,16 @@ bool influxDbWrite() {
     return true;
 }
 
+void suspendDevice() {
+    if (!bleIsConnected()) {
+        Serial.println(F("-->[IFDB] == shutdown =="));
+        Serial.flush();
+        powerDeepSleepTimer(DEEP_SLEEP_TIME);
+    } else {
+        Serial.println(F("-->[IFDB] BLE client connected, skip shutdown"));
+    }
+}
+
 void influxDbLoop() {
     static uint_fast64_t timeStamp = 0;
     if (millis() - timeStamp > cfg.stime * 2 * 1000) {
@@ -65,6 +76,7 @@ void influxDbLoop() {
             if (influxDbWrite()){
                 if(cfg.devmode) Serial.println("-->[IFDB] write done.");
                 gui.displayDataOnIcon();
+                suspendDevice();
             }
             else
                 Serial.printf("[E][IFDB] write error to %s@%s:%i \n",cfg.ifx.db.c_str(),cfg.ifx.ip.c_str(),cfg.ifx.pt);

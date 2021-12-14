b47d84496b7e68f58264a5ad1d31992e8224b7bc
Hpsaturn <hpsaturn@gmail.com>
2021-12-14 13:56:47 +0100

    Habilitado ADC en pruebas. Falta el porcentaje

--- lib/gui-utils-oled/src/battery.cpp
+++ lib/gui-utils-oled/src/battery.cpp
@@ -5,6 +5,9 @@
 *   B A T T E R Y   C H A R G E   S T A T U S   M E T H O D S
 ******************************************************************************/
 
+int vref = 931; //1100
+float curv = 0;
+
 // Battery level
 unsigned int chargeLevel = 0;
 
@@ -12,12 +15,29 @@ unsigned int chargeLevel = 0;
 const int IP5306_2 = 27;  // PIN2 IP5306
 const int IP5306_3 = 14;  // PIN3 IP5306
 unsigned int Rdelay = 0;
+      
 #endif
 
 void batteryInit() {
 #ifdef TTGO_TQ
     pinMode(IP5306_2, INPUT);
     pinMode(IP5306_3, INPUT);
+    #else
+    pinMode(ADC_PIN, INPUT);
+    pinMode(ADC_EN, OUTPUT);
+    digitalWrite(ADC_EN, HIGH);
+    delay(10); 
+    esp_adc_cal_characteristics_t adc_chars;
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+    //Check type of calibration value used to characterize ADC
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        Serial.printf("-->[BATT] ADC eFuse Vref:%u mV\n", adc_chars.vref);
+        vref = adc_chars.vref;
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        Serial.printf("-->[BATT] ADC Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
+    } else {
+        Serial.printf("-->[BATT] ADC Default Vref: %u mV\n", vref);
+    }
 #endif
 }
 
@@ -70,6 +90,13 @@ void batteryloop() {
             return;
         }
     }
+    #else
+    digitalWrite(ADC_EN, HIGH);
+    delay(10);                         // suggested by @ygator user in issue #2
+    uint16_t v = analogRead(ADC_PIN);
+    curv = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+    digitalWrite(ADC_EN, LOW);   // for possible issue: https://github.com/Xinyuan-LilyGO/TTGO-T-Display/issues/6
+    // return curv;
 #endif
 }
 
@@ -79,5 +106,5 @@ unsigned int getChargeLevel() {
     Serial.print(chargeLevel);
     Serial.println("%");
 #endif
-    return chargeLevel;
-}
+return chargeLevel;
+} 
--- lib/gui-utils-oled/src/battery.hpp
+++ lib/gui-utils-oled/src/battery.hpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
+#include <esp_adc_cal.h>
+#include "hal.hpp"
 
 #define BATTERY_LOOP_INTERVAL   1
+#define BATTERY_MIN_V 3.2
+#define BATTERY_MAX_V 4.1
+#define BATTCHARG_MIN_V 4.65
+#define BATTCHARG_MAX_V 4.88
 
 void batteryInit();
 

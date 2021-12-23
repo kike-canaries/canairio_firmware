63506e9633983e1a7b9096cc6254c7bbba1d4cb5
Hpsaturn <hpsaturn@gmail.com>
2021-12-23 11:12:00 +0100

    First version for M5StickCPlus (ENV2Hat working)

---
+++ lib/gui-utils-m5tft/src/hal.hpp
@@ -0,0 +1,23 @@
+// M5STICKC HAL DEFINTIONS
+
+#ifdef M5STICKCPLUS
+
+#define I2C_SDA_PIN 21
+#define I2C_SCL_PIN 22
+
+#ifndef TFT_DISPOFF
+#define TFT_DISPOFF 0x28
+#endif
+
+#ifndef TFT_SLPIN
+#define TFT_SLPIN 0x10
+#endif
+
+#define ADC_EN 14
+#define ADC_PIN 34
+#define BUTTON_R 37
+#define BUTTON_L 39
+
+#endif
+
+

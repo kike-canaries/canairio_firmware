610f6caca4b152815b0521033b4c81fea77f9918
Hpsaturn <hpsaturn@gmail.com>
2021-12-26 17:04:34 +0100

    fixed missing calls for M5 battery and AXP values

--- lib/gui-utils-tft/src/battery.cpp
+++ lib/gui-utils-tft/src/battery.cpp
@@ -4,6 +4,7 @@ int vref = 1100;
 float curv = 0;
 
 void setupBattADC() {
+    #ifndef M5STICKCPLUS
     esp_adc_cal_characteristics_t adc_chars;
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
     //Check type of calibration value used to characterize ADC
@@ -15,9 +16,11 @@ void setupBattADC() {
     } else {
         Serial.printf("-->[BATT] ADC Default Vref: %u mV\n", vref);
     }
+    #endif
 }
 
 void setupBattery() {
+    #ifndef M5STICKCPLUS
     /*
     ADC_EN is the ADC detection enable port
     If the USB port is used for power supply, it is turned on by default.
@@ -28,15 +31,21 @@ void setupBattery() {
     delay(10);                         // suggested by @ygator user in issue #2
     setupBattADC();
     delay(10);                         // suggested by @ygator user in issue #2
+    #endif
 }
 
 float battGetVoltage() {
+    #ifdef M5STICKCPLUS
+    if (battIsCharging()) curv =  M5.Axp.GetVBusVoltage();
+    else curv = M5.Axp.GetBatVoltage();
+    #else
     // setupBattery();
     digitalWrite(ADC_EN, HIGH);
     delay(10);                         // suggested by @ygator user in issue #2
     uint16_t v = analogRead(ADC_PIN);
     curv = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
     digitalWrite(ADC_EN, LOW);   // for possible issue: https://github.com/Xinyuan-LilyGO/TTGO-T-Display/issues/6
+    #endif
     return curv;
 }
 
@@ -59,10 +68,22 @@ uint8_t battCalcPercentage(float volts) {
     }
 }
 
-void battUpdateChargeStatus() {
-    // digitalWrite(LED_PIN, battIsCharging());
+bool battIsCharging() {
+    #ifdef M5STICKCPLUS
+    return M5.Axp.GetVBusCurrent() > 0;
+    #else
+    return curv > BATTERY_MAX_V + (BATTCHARG_MIN_V - BATTERY_MAX_V ) / 2;
+    #endif
 }
 
-bool battIsCharging() {
-    return curv > BATTERY_MAX_V + (BATTCHARG_MIN_V - BATTERY_MAX_V ) / 2;
+void battPrintValues() {
+    #ifdef M5STICKCPLUS
+    Serial.printf("-->[BATT] AXP Temp\t: %.1fC\n", M5.Axp.GetTempInAXP192());                                          //Get the temperature of AXP192
+    Serial.printf("-->[BATT] AXP Bat Volts\t: %.3fv  \tI: %.3fma\n", M5.Axp.GetBatVoltage(), M5.Axp.GetBatCurrent());  //Output voltage and current of Bat
+    Serial.printf("-->[BATT] AXP USB Volts\t: %.3fv  \tI: %.3fma\n", M5.Axp.GetVBusVoltage(), M5.Axp.GetVBusCurrent());  //Output current and voltage of USB
+    Serial.printf("-->[BATT] AXP 5V  Volts\t: %.3fv  \tI: %.3fma\n", M5.Axp.GetVinVoltage(), M5.Axp.GetVinCurrent());
+    Serial.printf("-->[BATT] AXP Bat power\t: %.3fmw\n", M5.Axp.GetBatPower());
+    #else
+    Serial.printf("-->[BATT] Bat Volts\t: %.3fv", curv);  //Output voltage
+    #endif
 }
--- lib/gui-utils-tft/src/battery.hpp
+++ lib/gui-utils-tft/src/battery.hpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+#ifdef M5STICKCPLUS
+#include <M5StickCPlus.h>
+#else
 #include <esp_adc_cal.h>
+#endif
 #include "hal.hpp"
 
 #define BATTERY_MIN_V 3.2
@@ -7,12 +11,10 @@
 #define BATTCHARG_MIN_V 4.65
 #define BATTCHARG_MAX_V 4.88
 
-void setupBattADC();
 void setupBattery();
 float battGetVoltage();
 uint8_t battCalcPercentage(float volts);
-void battUpdateChargeStatus();
 bool battIsCharging();
-void adcPowerOff();
+void battPrintValues();
 
 uint8_t _calcPercentage(float volts, float max, float min);

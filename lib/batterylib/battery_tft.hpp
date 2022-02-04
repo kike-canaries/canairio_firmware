8ff0b6109c25f851dfeef11f48d9fd8dc016febb
Hpsaturn <hpsaturn@gmail.com>
2022-02-05 00:37:23 +0100

    fixed batt limits for CanAirIO Bike version (TFT)

--- lib/batterylib/battery_tft.hpp
+++ lib/batterylib/battery_tft.hpp
@@ -8,8 +8,8 @@
 #define ADC_PIN 34
 
 #define BATTERY_MIN_V 3.4
-#define BATTERY_MAX_V 4.05
-#define BATTCHARG_MIN_V 4.15
+#define BATTERY_MAX_V 4.19
+#define BATTCHARG_MIN_V 4.21
 #define BATTCHARG_MAX_V 4.8
 
 class Battery_TFT : public Battery {

5e856f1a28893526d26b660babef82d53abef91c
roberbike <roberbike@gmail.com>
2022-02-12 11:15:55 +0100

    OLED ADC  first set. Working.

--- lib/batterylib/battery_oled.cpp
+++ lib/batterylib/battery_oled.cpp
@@ -4,7 +4,37 @@
 #ifndef M5STICKCPLUS
   #ifndef TTGO_TDISPLAY
 
+int vref = 1086;
+float curv = 0;
+
+void setupBattADC() {
+    esp_adc_cal_characteristics_t adc_chars;
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC1_CHANNEL_6, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
+    //Check type of calibration value used to characterize ADC
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        Serial.printf("-->[BATT] ADC eFuse Vref:%u mV\n", adc_chars.vref);
+        vref = adc_chars.vref;
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        Serial.printf("-->[BATT] ADC Two Point --> coeff_a:%umV coeff_b:%umV\n", adc_chars.coeff_a, adc_chars.coeff_b);
+    } else {
+        Serial.printf("-->[BATT] ADC Default Vref: %u mV\n", vref);
+    }
+}
+
+
 void Battery_OLED::init(bool debug) {
+   this->debug = debug;
+    /*
+    ADC_EN is the ADC detection enable port
+    If the USB port is used for power supply, it is turned on by default.
+    If it is powered by battery, it needs to be set to high level
+    */
+    pinMode(ADC_EN, OUTPUT);
+    digitalWrite(ADC_EN, HIGH);
+    delay(10);                         // suggested by @ygator user in issue #2
+    setupBattADC();
+    delay(10);                         // suggested by @ygator user in issue #2
+
 }
 
 float Battery_OLED::getVoltage() {

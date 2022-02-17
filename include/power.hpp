2548031d9283c128a3b509ec73c8fbd26a2cd146
Hpsaturn <hpsaturn@gmail.com>
2022-02-17 14:00:05 +0100

    refactor power methods and include flows

--- include/power.hpp
+++ include/power.hpp
@@ -1,19 +1,12 @@
-#ifndef _POWER_H
-#define _POWER_H
-
-#include <Arduino.h>
 #include <esp_bt_main.h>
 #include <esp_bt.h>
 #include <esp_wifi.h>
 #include "hal.hpp"
 #include "driver/rtc_io.h"
+#include <Batterylib.hpp>
 
-#ifndef TTGO_TDISPLAY 
-#include <GUIUtils.hpp>
-#endif
+#define DEEP_SLEEP_TIME 180       // seconds
 
 void powerDeepSleepButton();
 void powerDeepSleepTimer(int);
 void powerLightSleepTimer(int);
-
-#endif

4d218411aabbb77dbc00edbbfd46bb237bf13d39
Hpsaturn <hpsaturn@gmail.com>
2022-02-17 14:00:05 +0100

    refactor power methods and include flows

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,13 @@
  */
 
 #include <Arduino.h>
-
 #include <Watchdog.hpp>
 #include <ConfigApp.hpp>
 #include <GUILib.hpp>
 #include <Sensors.hpp>
 #include <bluetooth.hpp>
+#include <power.hpp>
 #include <wifi.hpp>
-#include <Batterylib.hpp>
 
 UNIT selectUnit = UNIT::NUNIT;
 UNIT nextUnit = UNIT::NUNIT;
@@ -229,20 +228,6 @@ void setup() {
     Serial.begin(115200);
     delay(400);
     Serial.println("\n== CanAirIO Setup ==\n");
-    
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Disable Brownout Detector 
-
-    if ( battery.getVoltage() < 3.3)
-    {
-        Serial.println("-->[Bat] Goto DeepSleep (curv to low)");
-        powerDeepSleepTimer(DEEP_SLEEP_TIME);
-    }
-
-    // set cpu speed low to save battery
-    setCpuFrequencyMhz(80);
-    Serial.print("CPU Speed: ");
-    Serial.print(getCpuFrequencyMhz());
-    Serial.println(" MHz");
     logMemory("INIT");
 
     // init app preferences and load settings

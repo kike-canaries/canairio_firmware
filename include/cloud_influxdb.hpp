7fed9edd487b8a6a55e3e6109fbe2eb8e36b081f
Hpsaturn <hpsaturn@gmail.com>
2022-02-22 10:30:03 +0100

    added the similar schema of publication for all clouds

--- include/cloud_influxdb.hpp
+++ include/cloud_influxdb.hpp
@@ -1,7 +1,5 @@
-#define IFX_MIN_PUBLISH_INTERVAL 30   // time in seconds
 #define IFX_RETRY_CONNECTION      5   // influxdb publish retry 
 #define IFX_ERROR_COUNT_MAX       5   // max error count before full ESP restart 
-#define WAIT_FOR_SENSOR          25   // seconds
 
 void influxDbInit();
 void influxDbLoop();

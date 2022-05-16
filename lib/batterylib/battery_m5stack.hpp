ff921eac2839858eefebc0dba661f3b9475d651a
Hpsaturn <hpsaturn@gmail.com>
2022-05-16 23:13:03 +0200

    fixed MAX value in charge mode

--- lib/batterylib/battery_m5stack.hpp
+++ lib/batterylib/battery_m5stack.hpp
@@ -8,7 +8,7 @@
 
 #define BATTERY_MIN_V 3.4
 #define BATTERY_MAX_V 4.04
-#define BATTCHARG_MIN_V 3.69
+#define BATTCHARG_MIN_V 4.06
 #define BATTCHARG_MAX_V 4.198
 
 class Battery_M5STACK : public Battery {

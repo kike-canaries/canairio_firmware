8c103a6a1537e3117008ea275d27d99cf5d4b5a8
Hpsaturn <hpsaturn@gmail.com>
2023-04-05 18:58:56 +0200

    fixed crash on build for C3 around ADC

--- lib/batterylib/battery.hpp
+++ lib/batterylib/battery.hpp
@@ -32,6 +32,7 @@ class Battery {
                 this->callback->onBatteryUpdate(curv, getCharge(), isCharging());
                 printValues();
             }
+            printValues();
         }
     }
 

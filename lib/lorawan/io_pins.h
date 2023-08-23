99d7f95c80124346ebfefee87e87a9a3d901dce0
roberbike <roberbike@gmail.com>
2023-08-23 10:24:46 +0200

    join acepted

---
+++ lib/lorawan/io_pins.h
@@ -0,0 +1,9 @@
+// PIN definition for module
+#define PIN_LMIC_NSS  18 
+#define PIN_LMIC_RST  14 
+#define PIN_LMIC_DIO0 26 
+#define PIN_LMIC_DIO1 33 
+#define PIN_LMIC_DIO2 32 
+
+#define LORA_TX_INTERVAL 300
+#define LMIC_LORA_SF  DR_SF12 // LORA Data rate

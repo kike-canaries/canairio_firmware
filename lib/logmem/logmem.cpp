4d94cee92c2ad1f1d12bbb56969f69fba80503f4
Hpsaturn <hpsaturn@gmail.com>
2024-05-26 21:56:36 +0200

    complete preferences refactor. First try using EasyPreferences lib

--- lib/logmem/logmem.cpp
+++ lib/logmem/logmem.cpp
@@ -3,7 +3,7 @@
 uint32_t heap_size = 0;
 
 void logMemory(const char* msg) {
-  if (!cfg.devmode) return;
+  if (!devmode) return;
   if (heap_size == 0) heap_size = ESP.getFreeHeap();
   heap_size = heap_size - ESP.getFreeHeap();
   Serial.printf("-->[HEAP] %s bytes used\t: %05db/%03dKb\r\n", msg, heap_size, ESP.getFreeHeap() / 1024);
@@ -12,7 +12,6 @@ void logMemory(const char* msg) {
 
 void logMemoryObjects(){
     Serial.printf("-->[HEAP] sizeof sensors\t: %05ub\r\n", sizeof(sensors));
-    Serial.printf("-->[HEAP] sizeof config \t: %05ub\r\n", sizeof(cfg));
     Serial.printf("-->[HEAP] sizeof GUI    \t: %05ub\r\n", sizeof(gui));
     Serial.printf("-->[HEAP] free memory   \t: %05ub\r\n", ESP.getFreeHeap());
 }
--- lib/logmem/logmem.hpp
+++ lib/logmem/logmem.hpp
@@ -1,6 +1,6 @@
-#include <ConfigApp.hpp>
 #include <GUILib.hpp>
 #include <Sensors.hpp>
-
+#include <ConfigApp.hpp>
+ 
 void logMemory(const char* msg);
 void logMemoryObjects();

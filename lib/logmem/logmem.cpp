bf5b8599db46b6e5b0c77b4f77cbc7a401f65aa4
Hpsaturn <hpsaturn@gmail.com>
2025-03-10 00:56:32 +0100

    added coredump code for try to find the crash issue

--- lib/logmem/logmem.cpp
+++ lib/logmem/logmem.cpp
@@ -1,7 +1,32 @@
 #include <logmem.hpp>
 #include "ConfigApp.hpp"
+#include "esp_core_dump.h"
 
 uint32_t heap_size = 0;
+
+void checkCoreDumpPartition() {
+  Serial.println("-->[INFO] Check core dump partition..");
+  esp_core_dump_init();
+  esp_core_dump_summary_t *summary =
+      static_cast<esp_core_dump_summary_t *>(malloc(sizeof(esp_core_dump_summary_t)));
+  if (summary) {
+    esp_err_t err = esp_core_dump_get_summary(summary);
+    if (err == ESP_OK) {
+      Serial.println("-->[INFO] Getting core dump summary ok.");
+      log_i("Getting core dump summary ok.");
+
+    } else {
+      log_e("Getting core dump summary not ok. Error: %d", (int)err);
+      log_e("Probably no coredump present yet.");
+      log_e("esp_core_dump_image_check() = %d", esp_core_dump_image_check());
+    }
+    free(summary);
+  }
+  else {
+    Serial.println("[E][INFO] Failed core dump summary malloc");
+    log_e("Failed core dump summary malloc");
+  }
+}
 
 void logMemory(const char* msg) {
   if (!devmode) return;

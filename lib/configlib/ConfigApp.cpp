6fe9e2f2d3e0756130ed7b3201fd82359282b8c2
Hpsaturn <hpsaturn@gmail.com>
2025-03-12 22:38:33 +0100

    now devmode only changes via CLI or BLE. (UART redirect)

--- lib/configlib/ConfigApp.cpp
+++ lib/configlib/ConfigApp.cpp
@@ -50,9 +50,9 @@ void init(const char app_name[]) {
     deviceId = getDeviceId();
     reload();
     // override with debug INFO level (>=3)
-#if CORE_DEBUG_LEVEL >= 1
-    devmode = true;
-#endif
+// #if CORE_DEBUG_LEVEL >= 1
+    // devmode = true;
+// #endif
     if (devmode) Serial.println("-->[CONF] debug is enable.");
 }
 

7a52a392ec56cd53e9016145362b643a5feaf42b
Hpsaturn <hpsaturn@gmail.com>
2025-03-23 00:12:18 +0100

    removed some warnnings aroudn cli

--- lib/configlib/ConfigApp.cpp
+++ lib/configlib/ConfigApp.cpp
@@ -431,7 +431,7 @@ String getDeviceId() {
     uint8_t baseMac[6];
     // Get MAC address for WiFi station
     esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-    char baseMacChr[18] = {0};
+    char baseMacChr[19] = {0};
     sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]+2);
     return String(baseMacChr);
 }

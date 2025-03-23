f92025f416c876be4be1d69b80a28108b2652d20
Hpsaturn <hpsaturn@gmail.com>
2025-03-23 22:29:44 +0100

    added restart when the ifx publish reach try count

--- lib/wifi/cloud_influxdb.hpp
+++ lib/wifi/cloud_influxdb.hpp
@@ -4,6 +4,7 @@
 
 #define IFX_RETRY_CONNECTION      5   // influxdb publish retry 
 #define IFX_ERROR_COUNT_MAX       5   // max error count before full ESP restart 
+#define IFX_RETRY_PUBLISH         3   // retry publish to influxdb
 
 void influxDbInit();
 void influxDbLoop();

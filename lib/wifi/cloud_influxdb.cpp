498faf0437e264dc9d0a382ad3b4005735649bce
roberbike <roberbike@gmail.com>
2025-12-18 08:42:49 +0100

    Add noise ito influx

--- lib/wifi/cloud_influxdb.cpp
+++ lib/wifi/cloud_influxdb.cpp
@@ -57,6 +57,12 @@ void influxDbParseFields() {
     sensor.addField("bat",battery.getCharge());
     sensor.addField("vbat",battery.getVoltage());
     #endif
+    sensor.addField("db",sensors.getNoise());
+    sensor.addField("dbAvg",sensors.getNoiseAverage());
+    sensor.addField("dbPk",sensors.getNoisePeak());
+    sensor.addField("dbMin",sensors.getNoiseMin());
+    sensor.addField("dbAvgL",sensors.getNoiseLegalAverage());
+    sensor.addField("dbAvgLMx",sensors.getNoiseLegalMaximum());
     sensor.addField("rssi",getWifiRSSI());
     sensor.addField("heap",ESP.getFreeHeap());
     sensor.addField("name",getStationName().c_str());

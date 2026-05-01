8139e41b1eb2f6fcf959fff6ed7984776d244fd5
Roberbto Fernandez <roberbike@gmail.com>
2026-05-01 22:05:56 +0200

    fix conflic with ADC_EN on T7-S3

--- lib/batterylib/battery_oled.hpp
+++ lib/batterylib/battery_oled.hpp
@@ -1,22 +1,20 @@
 #ifndef battery_oled_hpp
 #define battery_oled_hpp
-
 #include <battery.hpp>
 #include <esp_adc_cal.h>
-#if defined(TTGO_TDISPLAY)
-#define ADC_EN 14
-#endif
+
 class Battery_OLED : public Battery {
-  public:
-    void init(bool debug = false);
-    float getVoltage();
-    bool isCharging();
-    int getCharge();
-    void printValues();
-    void update();
-  private:
-    int vref = 1086;
-    void setupBattADC();
+public:
+  void init(bool debug = false);
+  float getVoltage();
+  bool isCharging();
+  int getCharge();
+  void printValues();
+  void update();
+
+private:
+  int vref = 1086;
+  void setupBattADC();
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_OLEDBATTERY)
@@ -24,4 +22,3 @@ extern Battery_OLED battery;
 #endif
 
 #endif
-

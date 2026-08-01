88ba150cb2b27068ceac63609321696eea53eb0e
Hpsaturn <hpsaturn@gmail.com>
2026-08-01 14:43:37 +0200

    changed to enable the ForceTxPower kset

--- lib/cli/cli.cpp
+++ lib/cli/cli.cpp
@@ -309,7 +309,7 @@ void initShell(){
   wcli.add("clear", &wcli_clear,        "\t\tclear shell");
   wcli.add("setup", &wcli_setup,        "\t\tTYPE THIS WORD to enter to SAFE MODE setup");
 
-  if (cfg.getBool(CONFKEYS::KFTXPWR, false)) wcli.forceTxPower(); // force the Tx power (C3 issue)
+  if (cfg.getBool(CONFKEYS::KFTXPWR, true)) wcli.forceTxPower(); // force the Tx power (C3 issue)
   
   wcli.begin("CanAirIO");
 }
